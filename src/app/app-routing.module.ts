0ae1f8fea41cd17d419050dd960e4cc86783f6a1
Cortelettim <cortelettimateu@hotmail.com>
2021-05-06 16:41:29 -0300

    Flexbox

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { FilmesComponent } from './components/view/filmes/filmes.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PersonagensComponent } from './components/view/personagens/personagens.component';
 
 const routes: Routes = [{
   path: '', component: FilmesComponent
-}];
+},{
+  path: 'personagens', component: PersonagensComponent
+}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
